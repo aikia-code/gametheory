9c3bc1e807fa7f6fb36f49535d918fe6251dd417
Isaac Antwi <prosperokay@gmail.com>
2024-01-27 13:29:04 +0000

    test(simulation): :white_check_mark: test strategy simulation + get strategy name

--- test/test_dilemma_models.py
+++ test/test_dilemma_models.py
@@ -4,6 +4,8 @@ from dilemma.models import Player, Session
 from dilemma.strategies import always_cooperate, always_defect
 from dilemma.symbols import COOPERATE, DEFECT
 from dilemma.symbols import TEMPT, SUCKER
+from dilemma.simulation import simulate_strategies
+from dilemma.strategies import get_strategy_name
 
 
 class TestDilemmaModels:
@@ -22,7 +24,7 @@ class TestDilemmaModels:
 
         assert self.player_inst.action == COOPERATE
 
-    def test_session_score_compute(self):
+    def test_session_computation(self):
         """test to assert score computation"""
 
         assert self.session_inst.players[0].name == "sand"
@@ -38,3 +40,20 @@ class TestDilemmaModels:
         assert self.session_inst.players[0].score == SUCKER[0]
 
         assert self.session_inst.players[1].score == TEMPT[0]
+
+    def test_strategy_simulation(self):
+        """test strategy simulation functions"""
+
+        simulation = simulate_strategies(
+            slot1=always_cooperate, slot2=always_defect, rounds=100
+        )
+
+        assert simulation.get_total_score(1) == 0
+
+        assert simulation.get_total_score(2) == 500
+
+        assert simulation.get_average_score(2) == 5
+
+        assert simulation.get_mode_score(2) == 5
+
+        assert get_strategy_name(always_defect) == "Always Defect strategy"
