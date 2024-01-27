e739cabdf15ab4577f06b3a846d5d8e331dbca8f
Isaac Antwi <prosperokay@gmail.com>
2024-01-27 13:31:14 +0000

    feat(simulation): :alembic: debug print simulation

--- dilemma/__main__.py
+++ dilemma/__main__.py
@@ -2,5 +2,10 @@ r"""module entry point
 """
 
 from .simulation import simulate_strategies
+from .strategies import always_cooperate, always_defect
 
-simulate_strategies()
+simulation = simulate_strategies(
+    slot1=always_cooperate, slot2=always_defect, rounds=100
+)
+
+print(simulation)
