f0a9da39897dce5b3a9e3f936d059ac2d32e596d
Isaac Antwi <prosperokay@gmail.com>
2024-01-27 13:32:22 +0000

    style(simulation): :art: use slots of strategy for simulation implementation

--- dilemma/simulation.py
+++ dilemma/simulation.py
@@ -6,22 +6,25 @@ from .models import Session, Simulation
 from .strategies import get_strategy_name
 
 
-def simulate_strategies(strategy1=None, strategy2=None, rounds=1):
+def simulate_strategies(slot1=None, slot2=None, rounds=1) -> Simulation:
     """run simulation by strategies
 
     Return:
         - (simulation): object containing all simulation information
     """
+    if slot1 is None or slot2 is None:
+        raise ValueError("Provide two-set of strategies")
+
     simulation = Simulation()
 
     for _ in range(rounds):
         session = Session(
-            names=(get_strategy_name(strategy1), get_strategy_name(strategy2))
+            names=(get_strategy_name(slot1), get_strategy_name(slot2))
         )
 
-        session.players[0].respond(strategy=strategy1)
+        session.players[0].respond(strategy=slot1)
 
-        session.players[1].respond(strategy=strategy2)
+        session.players[1].respond(strategy=slot2)
 
         session.compute_payoffs()
 
