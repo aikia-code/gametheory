8d2e8bb42148025cea9fc76c04b0fb5da5b35005
Isaac Antwi <prosperokay@gmail.com>
2024-01-28 20:11:47 +0000

    refactor(strategy): :art: move get strategy name to utils

--- dilemma/strategies.py
+++ dilemma/strategies.py
@@ -45,25 +45,6 @@ class RandomDefect(Strategy):
 
 
 # -----------------------------------------------------
-# helper
-
-
-def get_strategy_name(strategy=None):
-    """return the name of the strategy
-
-    Args:
-        strategy (strategy): strategy object
-
-    Returns:
-        str: name of strategy
-    """
-    if strategy is None:
-        raise ValueError("A strategy needs to be specified")
-
-    return strategy.__doc__.split(sep="\n")[0]
-
-
-# -----------------------------------------------------
 # exports
 
 
