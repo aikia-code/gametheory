a540b386245276d7e6a2e376189ac246bb81b0a4
Isaac Antwi <prosperokay@gmail.com>
2024-01-29 11:11:20 +0000

    chore: include version information

--- dilemma/__init__.py
+++ dilemma/__init__.py
@@ -1,2 +1,10 @@
-r"""module initialization
+r"""
+Interactive platform to simulate and engage with game theory
+
+Game theory is a key tool for analyzing strategic interactions and decision-making 
+across diverse areas. It enhances our comprehension of human behavior in real-world 
+situations, encompassing both cooperation and competition.
 """
+
+__version__ = "0.3.0"
+__title__ = "Game Theory"
