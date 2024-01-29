d04451adce4bafa6dae402708955958ccc9ebd8b
Isaac Antwi <prosperokay@gmail.com>
2024-01-29 13:05:52 +0000

    chore: update version number

--- dilemma/__init__.py
+++ dilemma/__init__.py
@@ -6,5 +6,5 @@ across diverse areas. It enhances our comprehension of human behavior in real-wo
 situations, encompassing both cooperation and competition.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.1.0"
 __title__ = "Game Theory"
