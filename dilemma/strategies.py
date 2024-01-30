ca38b361d120a0eb144f4c6411e8e00e090f5b74
Isaac Antwi <prosperokay@gmail.com>
2024-01-30 14:08:35 +0000

    feat: add tit for tat strategy

--- dilemma/strategies.py
+++ dilemma/strategies.py
@@ -18,6 +18,10 @@ class AlwaysCooperate(Strategy):
     Cooperates at every encounter
     """
 
+    def __init__(self, opponent_index=0 | 1, session_history=None):
+        super().__init__(opponent_index, session_history)
+        self.strategy_name = "Always Cooperate"
+
     def run(self):
         return COOPERATE
 
@@ -27,6 +31,10 @@ class AlwaysDefect(Strategy):
 
     Defects at every encounter
     """
+
+    def __init__(self, opponent_index=0 | 1, session_history=None):
+        super().__init__(opponent_index, session_history)
+        self.strategy_name = "Always Defect"
 
     def run(self):
         return DEFECT
@@ -38,10 +46,29 @@ class RandomDefect(Strategy):
     Defects randomly with every encounter
     """
 
+    def __init__(self, opponent_index=0 | 1, session_history=None):
+        super().__init__(opponent_index, session_history)
+        self.strategy_name = "Random"
+
     def run(self):
         player_choices = [COOPERATE, DEFECT]
 
         return choice(player_choices)
+
+
+class TitForTat(Strategy):
+    """Tit for tat strategy"""
+
+    def __init__(self, opponent_index=0 | 1, session_history=None):
+        super().__init__(opponent_index, session_history)
+        self.strategy_name = "Tit for Tat"
+
+    def run(self):
+        return (
+            DEFECT
+            if self.session_history[-1].players[self.opponent_index].action
+            else COOPERATE
+        )
 
 
 # -----------------------------------------------------
