622f03fc2464f4e3f3efbef62133fa4bd878854d
Isaac Antwi <prosperokay@gmail.com>
2024-01-30 14:08:09 +0000

    refactor: abstract setup process

--- dilemma/__main__.py
+++ dilemma/__main__.py
@@ -25,23 +25,8 @@ UI:
         -                         |   -   |    -    |   -  |
 
 """
+from .simulation import process_run_simulation, process_setup_simulation
 
-from .utils import (
-    get_strategy_name,
-    simulate_strategies,
-    update_statistics,
-    summarize_statistics,
-    tabulate_summary,
-)
-from .strategies import always_cooperate, always_defect, random_defect
-from .simulation import process_run_simulation
-
-
-strategies = {
-    1: always_cooperate,
-    2: always_defect,
-    3: random_defect,
-}
 
 print("Prisoner's dilemma")
 
@@ -56,34 +41,5 @@ while True:
         break
 
     if user_choice in ["b", "B", "2"]:
-        print("select strategy for slot1")
-        print(get_strategy_name(strategies[1]), "  --[1]")
-        print(get_strategy_name(strategies[2]), "  --[2]")
-        print(get_strategy_name(strategies[3]), "  --[3]")
-        slot1 = strategies[int(input("> "))]
-
-        print("select strategy for slot2")
-        print(get_strategy_name(strategies[1]), "  --[1]")
-        print(get_strategy_name(strategies[2]), "  --[2]")
-        print(get_strategy_name(strategies[3]), "  --[3]")
-        slot2 = strategies[int(input("> "))]
-
-        print("specify number of rounds")
-        number_of_rounds = int(input("> "))
-
-        simulation = simulate_strategies(slot1, slot2, number_of_rounds)
-
-        slot1_statistics = {"total": [], "average": [], "mode": []}
-        slot2_statistics = {"total": [], "average": [], "mode": []}
-
-        slot1_statistics = update_statistics(slot1_statistics, simulation, 1)
-        slot2_statistics = update_statistics(slot2_statistics, simulation, 2)
-
-        slot1_statistics = summarize_statistics(slot1_statistics)
-        slot2_statistics = summarize_statistics(slot2_statistics)
-
-        tabulate_summary()
-        tabulate_summary(slot1, slot1_statistics)
-        tabulate_summary(slot2, slot2_statistics)
-
+        process_setup_simulation()
         break
