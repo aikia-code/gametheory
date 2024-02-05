067a86168b146f360b32d380ed858bd1b1f0f1c5
Isaac Antwi <prosperokay@gmail.com>
2024-02-05 12:57:25 +0000

    feat(strategy): :sparkles: add `titfor2tat`, `historian`, and `unforgiving` strategies

--- dilemma/strategies.py
+++ dilemma/strategies.py
@@ -5,7 +5,7 @@ r"""A collection of available strategies for simulation
 from random import choice
 
 # standard simulation imports
-from .symbols import COOPERATE, DEFECT
+from .symbols import COOPERATE, DEFECT, PlayerAction
 from .models import Strategy
 
 
@@ -21,6 +21,7 @@ class AlwaysCooperate(Strategy):
 
     name = "Always Cooperate"
     statistics: dict = {"total": [], "average": [], "mode": []}
+    population = 2
 
     def run(self):
         return COOPERATE
@@ -34,6 +35,7 @@ class AlwaysDefect(Strategy):
 
     name = "Always Defect"
     statistics: dict = {"total": [], "average": [], "mode": []}
+    population = 2
 
     def run(self):
         return DEFECT
@@ -47,6 +49,7 @@ class RandomDefect(Strategy):
 
     name = "Random"
     statistics: dict = {"total": [], "average": [], "mode": []}
+    population = 2
 
     def run(self):
         player_choices = [COOPERATE, DEFECT]
@@ -59,6 +62,7 @@ class TitForTat(Strategy):
 
     name = "Tit for Tat"
     statistics: dict = {"total": [], "average": [], "mode": []}
+    population = 2
 
     def run(self):
         try:
@@ -67,3 +71,71 @@ class TitForTat(Strategy):
             return COOPERATE
         except IndexError:
             return COOPERATE
+
+
+class TitFor2Tat(Strategy):
+    """Tit for 2 tat strategy
+
+    Defects after opponent defects twice in a row
+    """
+
+    name = "Tit for 2 Tat"
+    statistics: dict = {"total": [], "average": [], "mode": []}
+    population = 2
+
+    def run(self):
+        try:
+            if (
+                self.session_history[-1].players[self.opponent_index].action == DEFECT
+                and self.session_history[-2].players[self.opponent_index].action
+                == DEFECT
+            ):
+                return DEFECT
+            return COOPERATE
+        except IndexError:
+            return COOPERATE
+
+
+class Unforgiving(Strategy):
+    """None Forgiving strategy
+
+    Defects forever if opponent defects even once
+    """
+
+    name = "Unforgiving"
+    statistics: dict = {"total": [], "average": [], "mode": []}
+    population = 2
+    no_mercy: bool = False
+
+    def run(self):
+        try:
+            if self.session_history[-1].players[self.opponent_index].action == DEFECT:
+                self.no_mercy = True
+        except IndexError:
+            return COOPERATE
+        return DEFECT if self.no_mercy else COOPERATE
+
+
+class Historian(Strategy):
+    """Historian strategy
+
+    Defects when opponent has defected more historically
+    """
+
+    name = "Historian"
+    statistics: dict = {"total": [], "average": [], "mode": []}
+    population = 2
+    opponent_action_history: list[PlayerAction] = []
+
+    def run(self):
+        try:
+            self.opponent_action_history.append(
+                self.session_history[-1].players[self.opponent_index].action
+            )
+        except IndexError:
+            return COOPERATE
+
+        total_cooperate = self.opponent_action_history.count(COOPERATE)
+        total_defect = self.opponent_action_history.count(DEFECT)
+
+        return DEFECT if total_defect > total_cooperate else COOPERATE
