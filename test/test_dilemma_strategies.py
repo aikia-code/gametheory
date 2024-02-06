61dd87f8c5473ad6954d445f3f4e0761940a3455
Isaac Antwi <prosperokay@gmail.com>
2024-02-06 00:13:12 +0000

    test(strategy): :white_check_mark: `tit for _ tat`

--- test/test_dilemma_strategies.py
+++ test/test_dilemma_strategies.py
@@ -1,12 +1,12 @@
+"""Test all strategies"""
+
 from dilemma.utils import (
     simulate,
-    summarize_statistics,
 )
 from dilemma.strategies import (
     AlwaysCooperate,
     AlwaysDefect,
     TitForTat,
-    Historian,
     TitFor2Tat,
 )
 
@@ -22,3 +22,24 @@ class TestDilemmaStrategies:
 
         simulation = simulate(AlwaysDefect([]), AlwaysDefect([]), 50)
         assert simulation.get_total_score(1) == simulation.get_total_score(2) == 50
+
+        simulation = simulate(AlwaysCooperate([]), AlwaysDefect([]), 50)
+        assert simulation.get_total_score(1) == 0
+        assert simulation.get_total_score(2) == 250
+
+    def test_tits_for_x_tats_strategies(self):
+        """Test the tit for tats strategies with match ups"""
+
+        simulation = simulate(TitFor2Tat([]), TitForTat([]), 50)
+        assert simulation.get_total_score(2) == simulation.get_total_score(1) == 150
+        assert simulation.get_mode_score(2) == simulation.get_mode_score(1) == 3
+
+        simulation = simulate(TitFor2Tat([]), AlwaysDefect([]), 50)
+        assert simulation.get_total_score(1) == 48
+        assert simulation.get_total_score(2) == 58
+        assert simulation.get_mode_score(2) == simulation.get_mode_score(1) == 1
+
+        simulation = simulate(TitForTat([]), AlwaysDefect([]), 50)
+        assert simulation.get_total_score(1) == 49
+        assert simulation.get_total_score(2) == 54
+        assert simulation.get_mode_score(2) == simulation.get_mode_score(1) == 1
